d0a3d560166f9ae4e356bbe8fe1e4df1f48dfd6a
nayakshubhankar16@gmail.com <nayakshubhankar16@gmail.com>
2025-05-28 20:58:59 +0530

    JWT added successfully

---
+++ src/main/java/com/crazycoder/security/JWTFilter.java
@@ -0,0 +1,44 @@
+package com.crazycoder.security;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.crazycoder.service.JWTService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@Component
+public class JWTFilter extends OncePerRequestFilter {
+	
+	@Autowired
+	private JWTService jwtService;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		String authHeader = request.getHeader("Authorization");
+		
+		String token= null;
+		String username=null;
+		
+		if(authHeader!=null && authHeader.startsWith("Bearer ")) {
+			token = authHeader.substring(7);
+			username = JWTService
+		}
+		
+	}
+
+	
+
+}
